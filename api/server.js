260bd325d206bdb9e25ffbbc78326ca32b557b5d
kato hawkins <kristhahawkins@icloud.com>
2021-07-26 17:21:57 -0400

    updated server.js

--- api/server.js
+++ api/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const helmet = require("helmet");
 
+const authRouter = require('./auth/auth-router.js')
+const usersRouter = require('./users/user-router.js')
 const RecipeRouter = require('./recipes/recipes-router')
 
 const server = express();
@@ -8,6 +10,16 @@ const server = express();
 server.use(helmet());
 server.use(express.json());
 
+server.use('/api/auth',authRouter)
+server.use('/api/users',userRouter)
 server.use("/api/recipes", RecipeRouter);
+
+
+server.use((err, req, res, next) => { // eslint-disable-line
+  res.status(err.status || 500).json({
+    message: err.message,
+    stack: err.stack,
+  });
+});
 
 module.exports = server;
