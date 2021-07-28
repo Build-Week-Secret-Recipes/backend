e8644c9d2642d7979489c9b4f4746732b5fbd577
kato hawkins <kristhahawkins@icloud.com>
2021-07-28 17:02:03 -0400

    updated seed files

--- data/seeds/02-recipes.js
+++ data/seeds/02-recipes.js
@@ -1,7 +1,8 @@
 exports.seed = function(knex,Promise){
     return knex('recipes').insert([
-        {recipe_name:'Spaghetti'},
-        {recipe_name:'Hamburgers'},
-        {recipe_name:'Sushi'}
+      
+        { users_id: 1, recipe_name: 'Spaghetti', description: "Don't toucha my Spaghet", prep_time: '15 minutes', cook_time: '15 minutes' },
+        { users_id: 2, recipe_name: 'Hamburgers', description: 'Learn how to make delicious, juicy hamburgers', prep_time: '10 minutes', cook_time: '10 minutes' },
+        { users_id: 3, recipe_name: 'Sushi', description: 'Learn how to make tasty soosh', prep_time: '20 minutes', cook_time: '20 minutes' }
     ]);
 };
