8efd28417974de70d44da33881a0165264e7b1a1
kato hawkins <kristhahawkins@icloud.com>
2021-07-30 21:36:00 -0400

    added github link

--- api/server.js
+++ api/server.js
@@ -11,7 +11,8 @@ server.use(helmet());
 server.use(express.json());
 
 server.get('/', (req, res) => {
-  res.send(`<h2>api is up! Start by running /api/recipes to get recipes to get recipes!</h2>`);
+  res.send(`<h2>api is up! Start by running /api/recipes to get recipes to get recipes!
+  https://github.com/Build-Week-Secret-Recipes/backend</h2>`);
 });
 server.use('/api/auth',authRouter)
 server.use('/api/users',usersRouter)
